984b8f547f7de7a33aaaa877fa85c8861436aa71
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-11-28 01:14:09 +0530

    [Ignore]

--- ttpoint.py
+++ ttpoint.py
@@ -26,4 +26,90 @@ print(str[1:10])  # Python operators for slice([] and [:])
 list = ["Groceries", 100, "Mobile Phone", ["Earphones", "Cricket Bat"]]
 print(list[3][0])
 
+""" Python Tuples: They are similar to lists but have the following differences
+    1) Tuples are enclosed by ( ) 
+    2) We cannot update the tuples unlike lists, so they can be called as read-only lists"""
+tuple = ("Groceries", 100, "Mobile Phone", ("Earphones", "Cricket Bat"))
+print(tuple)
+
+""" Python dictionary: There are similar to hash tables i.e key and value pairs """
+my_dictionary = {
+ 	               1 : "Number 1",
+ 	               2 : "Number 2",
+ 	               "three" : 3
+                }
+print(my_dictionary["three"])
+
+""" Data type conversions """
+#int 
+x = int(17.2)
+print(x)
+
+""" Some of Python Arithmetic Operators """
+
+#Division
+d = 4/3
+print(d) #The output can result a float point, for this floor division can be used
+
+#Floor Division
+f = 4//3
+print(f) #returns floor value
+
+#Exponent
+e = 4**2
+print(e)
+
+""" Identity Operator: Python built-in function id() returns a unique integer as identity of object.
+    *** Refer to the pythons memory and execution model :
+    Link: https://jeffknupp.com/blog/2013/02/14/drastically-improve-your-python-understanding-pythons-execution-model/ """
+
+a = 20
+b = 20
+print ('Line 1','a=',a,':',id(a), 'b=',b,':',id(b))
+
+if ( a is b ):
+   print ("Line 2 - a and b have same identity")
+else:
+   print ("Line 2 - a and b do not have same identity")
+
+if ( id(a) == id(b) ):
+   print ("Line 3 - a and b have same identity")
+else:
+   print ("Line 3 - a and b do not have same identity")
+
+b = 30
+print ('Line 4','a=',a,':',id(a), 'b=',b,':',id(b))
+
+if ( a is not b ):
+   print ("Line 5 - a and b do not have same identity")
+else:
+   print ("Line 5 - a and b have same identity")
+# In python here 20 object is created and assigned to variable and when it changes to 20 the id() value also changes. It's execution varies from others.
+
+""" Loop Statements:
+    Python supports the following Control Statements 1) break 2) continue 3) pass """
+#Continue statement
+for letter in 'Python':
+   if letter == 'h':
+      continue
+   print ('Current Letter :', letter)
+
+"""Pass statement: It is a null operation, it does execute anything but is rather used to acheive syntactical correctness. It simple acts as a 
+place holder for the methods et cetera, which are not yet there"""
+
+for letter in 'Python': 
+   if letter == 'h':
+      pass
+      print ('This is pass block')
+   print ('Current Letter :', letter)
+
+print ("Good bye!")
+
+
+
+    
+
+
+
+
 
