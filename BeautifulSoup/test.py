83af1a6c9df7e93e098baf0dade3ad74fb1558d2
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-02 23:51:21 +0530

    [ Updated Beautiful Soup ]

--- BeautifulSoup/test.py
+++ BeautifulSoup/test.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 
 import urllib.request as urlRequest
 
-url = "http://www.hdwallpapers.in/"
+url = "http://www.hdwallpapers.in"
 
 # headers = {'User-Agent': 'Mozilla/5.0'}
 
@@ -10,9 +10,21 @@ req = urlRequest.Request(url)
 
 html_content = urlRequest.urlopen(req)
 
-soup = BeautifulSoup(html_content.read(),"html.parser") 
+soup = BeautifulSoup(html_content.read(), "html.parser")
 
-wallpapers = soup.find_all('div',class_="thumbbg")
+links = soup.find_all('a')
 
-print(soup.prettify)
+file = open("wallpaper_links.txt","w")
 
+""" # to extract all the urls through anchor tags 
+for link in links:
+	print ("<a href= '%s' > %s </a>"%(link.get("href"),link.text)) """
+
+image_links = soup.find_all('div', class_="thumb")
+
+for image_link in image_links:
+	for each_it in image_link:
+		print("Writing:",each_it.text)
+		file.write(url+each_it.get("href")+"\n")
+
+file.close()
