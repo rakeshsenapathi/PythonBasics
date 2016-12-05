5616fccad3debbdbf93a62304f756745da55ae9f
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-05 22:14:05 +0530

    [Ignore]

--- BeautifulSoup/GSMArena_Scrape.py
+++ BeautifulSoup/GSMArena_Scrape.py
@@ -2,26 +2,38 @@
 
 import requests
 
+import webbrowser
+
+import sys
+
 from bs4 import BeautifulSoup
 
-url = "http://www.gsmarena.com/" #base url
+url = "http://www.gsmarena.com/"  # base url
 
 req = requests.get(url)
 
 # status_code=200 represents OK
 if(req.status_code != 200):
-	print(req.status_code)
+    print(req.status_code)
 
 soup = BeautifulSoup(req.content, "html.parser")
 
 lists = soup.find('div', class_="brandmenu-v2 light l-box clearfix")
 
-phone_list = lists.find_all('li')
+brand_list = lists.find_all('li')
+links = lists.find_all('a')
 
-for each_phone in phone_list:
-	print(each_phone.text)
+name = input("Enter Mobile brand: ")
+for each_link in links:
+    if(name == (each_link.text)):
+        print("Brand exists....")
+        print("Opening url....")
+        try:
+            webbrowser.open(url + each_link.get('href'))
+            sys.exit()
+        except Exception as e:
+            raise e
+    else:
+    	continue
 
-
-
-
-
+print("Brand doesnt exist")
