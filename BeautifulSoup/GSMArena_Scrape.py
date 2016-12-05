8ee0429024f84582b0e96dbdcb3643fab5b3c51f
rakeshsenapathi <rakeshsenapathi@gmail.com>
2016-12-05 20:33:21 +0530

    [Scraping to get list of phones done]

--- BeautifulSoup/GSMArena_Scrape.py
+++ BeautifulSoup/GSMArena_Scrape.py
@@ -1,18 +1,27 @@
+# Scraping home page of GSMArena to find the list of all phone brands
+
 import requests
 
 from bs4 import BeautifulSoup
 
-url = "http://www.gsmarena.com/"
+url = "http://www.gsmarena.com/" #base url
 
 req = requests.get(url)
 
-print(req.status_code)
+# status_code=200 represents OK
+if(req.status_code != 200):
+	print(req.status_code)
 
 soup = BeautifulSoup(req.content, "html.parser")
 
+lists = soup.find('div', class_="brandmenu-v2 light l-box clearfix")
 
-lists = soup.find_all('div', class_="brandmenu-v2 light l-box clearfix")
+phone_list = lists.find_all('li')
 
-print(type(lists))
-for list in lists:
-    print(type(list.contents[3]))
+for each_phone in phone_list:
+	print(each_phone.text)
+
+
+
+
+
